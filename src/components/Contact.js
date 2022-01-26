5f290bb1d9eceb81d3570cd61fdf63317fe6d3b7
Lewis Williams <lewisowilliams@gmail.com>
2022-01-26 14:47:11 +0000

    Fixed Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,11 +8,11 @@ const Contact = () => {
   const [message, setMessage] = React.useState("");
 
   function encode(data) {
-  return Object.keys(data)
-    .map(
-      (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-    )
-    .join("&");
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
   }
 
   function handleSubmit(e) {
@@ -26,68 +26,131 @@ const Contact = () => {
       .catch((error) => alert(error));
   }
 
-
-
   return (
-  <section id="contact">
-    <div className=" place-content-center container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap ">
-      <form
-        netlify
-        name="contact"
-        onSubmit={handleSubmit}
-        className="lg:w-1/3 md:w-1/2 md:py-8 mt-8 md:mt-0">
+    <section id="contact">
+      <div className="place-content-center container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+        <form
+          netlify
+          name="contact"
+          onSubmit={handleSubmit}
+          className="lg:w-1/3 md:w-1/2 md:py-8 mt-8 md:mt-0">
         <div className="text-center mb-20">
           <SiMaildotru size={30} className="w-10 inline-block mb-4" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
             Contact Me
           </h1>
         </div>
-        <div className="relative mb-4">
-          <label htmlFor="name" className="leading-7 text-sm text-gray-400">
-            Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div className="relative mb-4">
-          <label htmlFor="email" className="leading-7 text-sm text-gray-400">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="relative mb-4">
-          <label
-            htmlFor="message"
-            className="leading-7 text-sm text-gray-400">
-            Message
-          </label>
-          <textarea
-            id="message"
-            name="message"
-            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-            onChange={(e) => setMessage(e.target.value)}
-          />
-        </div>
-        <button
-          type="submit"
-          className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-          Submit
-        </button>
-      </form>
-    </div>
-  </section>
-  ); 
+
+          <div className="relative mb-4">
+            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="message"
+              className="leading-7 text-sm text-gray-400">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+
+
+
 }
 
 export default Contact;
+
+  // return (
+  // <section id="contact">
+  //   <div className=" place-content-center container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap ">
+  //     <form
+  //       netlify
+  //       name="contact"
+  //       onSubmit={handleSubmit}
+  //       className="lg:w-1/3 md:w-1/2 md:py-8 mt-8 md:mt-0">
+  //       <div className="text-center mb-20">
+  //         <SiMaildotru size={30} className="w-10 inline-block mb-4" />
+  //         <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+  //           Contact Me
+  //         </h1>
+  //       </div>
+  //       <div className="relative mb-4">
+  //         <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+  //           Name
+  //         </label>
+  //         <input
+  //           type="text"
+  //           id="name"
+  //           name="name"
+  //           className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+  //           onChange={(e) => setName(e.target.value)}
+  //         />
+  //       </div>
+  //       <div className="relative mb-4">
+  //         <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+  //           Email
+  //         </label>
+  //         <input
+  //           type="email"
+  //           id="email"
+  //           name="email"
+  //           className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+  //           onChange={(e) => setEmail(e.target.value)}
+  //         />
+  //       </div>
+  //       <div className="relative mb-4">
+  //         <label
+  //           htmlFor="message"
+  //           className="leading-7 text-sm text-gray-400">
+  //           Message
+  //         </label>
+  //         <textarea
+  //           id="message"
+  //           name="message"
+  //           className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+  //           onChange={(e) => setMessage(e.target.value)}
+  //         />
+  //       </div>
+  //       <button
+  //         type="submit"
+  //         className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+  //         Submit
+  //       </button>
+  //     </form>
+  //   </div>
+  // </section>
+  // ); 
